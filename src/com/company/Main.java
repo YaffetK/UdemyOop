e78c257cf7242d472655c6505c9befa434351a5f
yaffet97 <yaffet97@gmail.com>
2022-02-10 20:39:07 +0100

    Yheeez

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        BankAccount account=new BankAccount();
+        /*BankAccount account=new BankAccount();
 
         account.setBalance(1000);
         account.deposit(2000);
         account.deposit(5000);
 
-        System.out.println(account.getBalance());
+        System.out.println(account.getBalance());*/
+
+        Wall wall=new Wall(15,3);
+        System.out.println(wall.getHeight());
+        System.out.println(wall.getArea());
 
     }
 }
