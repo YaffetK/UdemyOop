7c254dcf4bfde9add9ef0a961f9e67044569c789
yaffet97 <yaffet97@gmail.com>
2022-02-10 20:23:24 +0100

    Yheeez

--- src/com/company/Wall.java
+++ src/com/company/Wall.java
@@ -23,4 +23,32 @@ public class Wall {
         }
 
     }
+
+    public double getWidth(){
+        return this.width;
+    }
+    public double getHeight(){
+        return this.height;
+    }
+
+    public void setWidth(double width){
+        if (width<0){
+            this.width=0;
+        }
+        else {
+            this.width = width;
+        }
+    }
+
+    public void setHeight(double height){
+        if (height<0){
+            this.height=0;
+        }
+        else {
+            this.height = height;
+        }
+    }
+    public double getArea(){
+        return height*width;
+    }
 }
