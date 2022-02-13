d6aa4418e797f3dbfd98f0668c48d4412b8cbd84
yaffet97 <yaffet97@gmail.com>
2022-02-13 11:57:26 +0100

    yheez

--- src/com/company/Point.java
+++ src/com/company/Point.java
@@ -29,6 +29,14 @@ public class Point {
     }
 
     public double distance(){
-        return Math.sqrt((0-0)*(0-0) + (y-x)* (y-x));
+        return Math.sqrt(getX()*getX() + (getY() * getY()));
+    }
+
+    public double distance(int x, int y){
+        return Math.sqrt((x-getX())*(x-getX()) + (y-getY())*(y-getY()));
+    }
+
+    public double distance(Point point){
+        return distance(point.x, point.y);
     }
 }
